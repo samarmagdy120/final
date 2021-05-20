1d28d50cf583c915112dd8c4c02126cbf59b3ff3
samarmagdy120 <samarmagdy120@yahoo.com>
2021-05-20 19:49:36 +0200

    Update Profilecraftman.js

--- src/Components/profilecraftman/Profilecraftman.js
+++ src/Components/profilecraftman/Profilecraftman.js
@@ -85,6 +85,7 @@ const Profilecraftman = ({ match }) => {
                   float: "right",
                   textAlign: "right",
                   marginTop: "25px",
+                  marginRight: "53px";
                 }}
               >
                 <li>
